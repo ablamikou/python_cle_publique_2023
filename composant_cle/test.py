8994d6d78d9bf9da9d91814b48dd5be6192f4cdc
ablamikou <abla.mikou@dauphine.eu>
2023-05-22 01:04:23 +0200

    Ajout print test

--- composant_cle/test.py
+++ composant_cle/test.py
@@ -7,7 +7,9 @@ macle = cle_component.Cle()
 macle.initialize("4b8e29b9b0dddd58a709edba7d6df6c07ebdaf5653e325114bc5318c238f87f0")
 
 # Récupérez la clé privée
+print("Cle prive : ")
 print(macle.getPrivateKey())
 
 # Récupérez la clé publique
+print("Cle public : ")
 print(macle.getPublicKey())
